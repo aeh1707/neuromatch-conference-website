dcfc8ff37dca2cc1bfbc7ab5388325da7dc91804
aeh1707d <abdelhadi.hireche@outlook.com>
2022-05-28 09:09:18 +0100

    Fix the issues with styles and functionality

--- script.js
+++ script.js
@@ -20,62 +20,57 @@ x.addEventListener('click', () => {
   logo.src = 'graphics/neuromatch-conference-light.svg';
 });
 
-const firstSpeaker = {
-  imgSrc: 'graphics/athena.jpeg',
-  name: 'Athena Akrami',
-  oraganization: 'University College London',
-  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-};
+const speakers = [
+  {
+    imgSrc: 'graphics/athena.jpeg',
+    name: 'Athena Akrami',
+    oraganization: 'University College London',
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  },
+  {
+    imgSrc: 'graphics/yuki.png',
+    name: 'Yuki Kamitani',
+    oraganization: 'Kyoto University',
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  },
+  {
+    imgSrc: 'graphics/bassett.jpeg',
+    name: 'Dani Bassett',
+    oraganization: 'University of Pennsylvania',
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  },
+  {
+    imgSrc: 'graphics/vivek.jpeg',
+    name: 'Vivek Jayaraman',
+    oraganization: 'Janelia Research Campus',
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  },
+  {
+    imgSrc: 'graphics/chethan.png',
+    name: 'Chethan Pandarinath',
+    oraganization: 'Georgia Tech/Emory',
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  },
+  {
+    imgSrc: 'graphics/bassett.jpeg',
+    name: 'Dani Bassett',
+    oraganization: 'University of Pennsylvania',
+    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  }
+];
 
-document.querySelector('.speaker-1 .speaker-img').src = firstSpeaker.imgSrc;
-document.querySelector('.speaker-1 h3').textContent = firstSpeaker.name;
-document.querySelector('.speaker-1 .speaker-subheading').textContent = firstSpeaker.oraganization;
-document.querySelector('.speaker-1 p').textContent = firstSpeaker.bio;
+const speakersContainer = document.querySelector('.speakers-container');
+const speakerArticle = speakers.map((speaker) => `<article class="speaker">
+<div class="speaker-img-container">
+    <img class="brain" src="graphics/brain-circuit.png" alt="">
+    <img class="speaker-img" src="${speaker.imgSrc}" alt="">
+</div>
+<div class="speaker-content">
+  <h3>${speaker.name}</h3>
+    <div class="speaker-subheading">${speaker.oraganization}</div>
+    <hr>
+    <p>${speaker.bio}</p>
+</div>
+</article>`).join('');
 
-const secondSpeaker = {
-  imgSrc: 'graphics/yuki.png',
-  name: 'Yuki Kamitani',
-  oraganization: 'Kyoto University',
-  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-};
-
-document.querySelector('.speaker-2 .speaker-img').src = secondSpeaker.imgSrc;
-document.querySelector('.speaker-2 h3').textContent = secondSpeaker.name;
-document.querySelector('.speaker-2 .speaker-subheading').textContent = secondSpeaker.oraganization;
-document.querySelector('.speaker-2 p').textContent = secondSpeaker.bio;
-
-const thirdSpeaker = {
-  imgSrc: 'graphics/bassett.jpeg',
-  name: 'Dani Bassett',
-  oraganization: 'University of Pennsylvania',
-  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-};
-
-document.querySelector('.speaker-3 .speaker-img').src = thirdSpeaker.imgSrc;
-document.querySelector('.speaker-3 h3').textContent = thirdSpeaker.name;
-document.querySelector('.speaker-3 .speaker-subheading').textContent = thirdSpeaker.oraganization;
-document.querySelector('.speaker-3 p').textContent = thirdSpeaker.bio;
-
-const fourthSpeaker = {
-  imgSrc: 'graphics/vivek.jpeg',
-  name: 'Vivek Jayaraman',
-  oraganization: 'Janelia Research Campus',
-  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-};
-
-document.querySelector('.speaker-4 .speaker-img').src = fourthSpeaker.imgSrc;
-document.querySelector('.speaker-4 h3').textContent = fourthSpeaker.name;
-document.querySelector('.speaker-4 .speaker-subheading').textContent = fourthSpeaker.oraganization;
-document.querySelector('.speaker-4 p').textContent = fourthSpeaker.bio;
-
-const fifthSpeaker = {
-  imgSrc: 'graphics/chethan.png',
-  name: 'Chethan Pandarinath',
-  oraganization: 'Georgia Tech/Emory',
-  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-};
-
-document.querySelector('.speaker-5 .speaker-img').src = fifthSpeaker.imgSrc;
-document.querySelector('.speaker-5 h3').textContent = fifthSpeaker.name;
-document.querySelector('.speaker-5 .speaker-subheading').textContent = fifthSpeaker.oraganization;
-document.querySelector('.speaker-5 p').textContent = fifthSpeaker.bio;
+speakersContainer.innerHTML += speakerArticle;
